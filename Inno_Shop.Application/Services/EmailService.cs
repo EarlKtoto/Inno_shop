7a5654f179007f9fca6ea2fcee8a0e3f3e5c4bc4
EarlKtoto <earlktoto@email.com>
2025-12-01 03:03:19 +0300

    Inno_Shop 0.9

--- Inno_Shop.Application/Services/EmailService.cs
+++ Inno_Shop.Application/Services/EmailService.cs
@@ -1,7 +1,9 @@
-using System.Net;
-using System.Net.Mail;
+using MailKit.Net.Smtp;
+using MailKit.Security;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 using MimeKit;
+using MimeKit.Text;
 
 namespace Application.Services;
 
@@ -16,19 +18,17 @@ public class EmailService
 
     public async Task SendEmailAsync(string email, string subject, string body)
     {
-        var message = new MailMessage();
-        message.From = new MailAddress(_configuration["Email:SenderEmail"]);
-        message.To.Add(email);
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress(_configuration["EmailSettings:MailName"], _configuration["EmailSettings:MailAddress"]));
+        message.To.Add(MailboxAddress.Parse(email));
         message.Subject = subject;
-        message.Body = body;
-        message.IsBodyHtml = true;
+        message.Body = new TextPart(TextFormat.Html) { Text = body };
+        
+        using var client = new SmtpClient();
+            await client.ConnectAsync(_configuration["EmailSettings:Host"], int.Parse(_configuration["EmailSettings:Port"]), SecureSocketOptions.StartTls);
+            await client.AuthenticateAsync(_configuration["EmailSettings:MailAddress"], _configuration["EmailSettings:Password"]);
+            await client.SendAsync(message);
 
-        using var smtp = new SmtpClient(_configuration["Email:Host"], int.Parse(_configuration["Email:Port"]))
-        {
-            Credentials = new NetworkCredential(_configuration["Email:SenderEmail"], _configuration["Email:Password"]),
-            EnableSsl = bool.Parse(_configuration["Email:EnableSsl"])
-        };
-
-        await smtp.SendMailAsync(message);
+        await client.DisconnectAsync(true);
     }
 }
